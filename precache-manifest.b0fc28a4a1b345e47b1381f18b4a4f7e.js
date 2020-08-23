0626b3f73172c11bcec9a44d099c8e3c36223814
chetvishal <40429237+chetvishal@users.noreply.github.com>
2020-08-23 19:13:58 +0530

    initial commit
    pwa initialised

---
+++ precache-manifest.b0fc28a4a1b345e47b1381f18b4a4f7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34601f0a4019bb6ffd0bf6836f48d241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22fbe43bff5a1d0c2b67",
+    "url": "/static/css/2.c98993d0.chunk.css"
+  },
+  {
+    "revision": "4bfdd099b27c37ac3b98",
+    "url": "/static/css/main.ba777428.chunk.css"
+  },
+  {
+    "revision": "22fbe43bff5a1d0c2b67",
+    "url": "/static/js/2.8577e42b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8577e42b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfdd099b27c37ac3b98",
+    "url": "/static/js/main.3012784b.chunk.js"
+  },
+  {
+    "revision": "cdefb25c8d703a7c52b4",
+    "url": "/static/js/runtime-main.613a80f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
